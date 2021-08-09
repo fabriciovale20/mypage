26b880c0a18fc16a76b1c896c5cb1c795fdf0116
FabricioVale <fabriciovale18@gmail.com>
2021-08-09 14:22:30 -0300

    Página Web (Filtro/Busca)

--- myapp/admin.py
+++ myapp/admin.py
@@ -13,10 +13,3 @@ class CadastroAdmin(admin.ModelAdmin):
     list_display = ('categoria', 'marca', 'modelo', 'macsn_name', 'valor', 'imagem', 'situacao')
     search_fields = ('macsn_name',)
     list_filter = ('modelo',)
-    # list_editable = ('imagem',)
-
-    # def get_register_date(self, obj):
-    #     if obj.register_date:
-    #         return obj.register_date.strftime('%d/%m/%Y')
-    #
-    # get_register_date.short_description = 'Data de publicação'
